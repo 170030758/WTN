71e570d2201634cf8785bf05fdf77b37d1f1ea32
170030758 <59186206+170030758@users.noreply.github.com>
2020-01-27 13:58:50 +0530

    Add files via upload

---
+++ p13.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+class p13
+{
+public static void main(String args[])
+{
+System.out.println("The sum of 10 and 20 is: "+(Integer.parseInt(args[0])+Integer.parseInt(args[1])));
+}
+}
